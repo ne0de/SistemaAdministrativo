4272120533642ef0c87aaa8b25255759d0392f47
nenedodo <nani.andres@hotmail.com.ar>
2021-11-12 17:36:57 -0300

    Agregado chequeo de deposito
    También muestreo de solicitudes de compra.

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-#include "Sistema.h"
-#include "Compras.h"
+#include "src/Sistema.h"
+#include "src/Compras.h"
 
 int main(int argc, char *argv[])
 {
     int Resultado, Opcion;
     struct NodoProducto *ListaProductos = GenerarListaProductos();
+    struct NodoSolicitudCompra *ListaSolicitudCompra = GenerarListaSolicitudCompra();
 
     Resultado = CargarProductos(&ListaProductos);
     if (Resultado == FALLO)
@@ -15,16 +16,15 @@ int main(int argc, char *argv[])
         Menu();
         scanf("%d", &Opcion);
 
-        if (Opcion == 2)
-            ProcesoIngresarProducto(&ListaProductos);
-
-        if (Opcion == 3)
-            MostrarProductos(ListaProductos);
+        if (Opcion == 1) ChequearDeposito(ListaProductos, &ListaSolicitudCompra);
+        if (Opcion == 2) ProcesoIngresarProducto(&ListaProductos);
+        if (Opcion == 3) MostrarProductos(ListaProductos);
+        if (Opcion == 4) MostrarSolicitudesDeCompra(ListaSolicitudCompra);
 
         puts("> Presiona una tecla para continuar..");
         fflush(stdin);
         getchar();
         system("cls");
-    } while (Opcion > 1 && Opcion < 3);
+    } while (Opcion > 0 && Opcion < 5);
     return 0;
 }
