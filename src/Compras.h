cdebc1fc520e887980d08a417cb4d29dca5fe6ac
nenedodo <nani.andres@hotmail.com.ar>
2021-11-12 17:37:23 -0300

    Update Compras.h

--- src/Compras.h
+++ src/Compras.h
@@ -3,7 +3,36 @@
 
 #include "Sistema.h"
 
-int ChequearDeposito(void);
-int GenerarSolicitudCompra();
+#define ARCH_PEDIDOS_REPOSICION "PedidosReposicion.nag"
+
+// Definición básica de una solicitud de compra.
+struct SolicitudCompra
+{
+    int Categoria, Cantidad;
+};
+
+// Lista de solicitudes simplemente enlazada.
+struct NodoSolicitudCompra
+{
+    struct SolicitudCompra Solicitud;
+    struct NodoSolicitudCompra *Siguiente;
+};
+
+// Sector de Depósito
+
+// Genera y devuelve una lista de solicitudes de compra.
+struct NodoSolicitudCompra *GenerarListaSolicitudCompra(void);
+
+// Dado una solicitud lo inserta en la lista de solicitudes de compra.
+int InsertarSiguienteSolicitud(struct NodoSolicitudCompra **, struct SolicitudCompra);
+
+// Detecta la necesidad de reponer un producto, si es asi genera una nueva solicitud de compra.
+int ChequearDeposito(struct NodoProducto *, struct NodoSolicitudCompra **);
+
+// Muestra una unica solicitud de compra en la consola.
+void MostrarUnicaSolicitudDeCompra(struct SolicitudCompra);
+
+// Itera todos las solicitudes de compras para mostrarlos en la consola.
+void MostrarSolicitudesDeCompra(struct NodoSolicitudCompra *);
 
 #endif
